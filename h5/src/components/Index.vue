5c30b310b73dda6245b4c6c0c0b72ab931035d49
Wenqiang Li <wenqiangli@WenqiangtekiMacBook-Pro.local>
2017-07-02 11:20:30 +0800

    misc

---
+++ h5/src/components/Index.vue
@@ -0,0 +1,23 @@
+<template>
+	<div class="index">
+		{{name}}
+	</div>
+</template>
+
+<script>
+	export default{
+		name: 'index',
+		data () {
+			return {
+				name: 'index'
+			}
+		}
+	}
+</script>
+
+<style scoped lang="less">
+	@black: #000;
+	.index{
+		background: @black;
+	}
+</style>
